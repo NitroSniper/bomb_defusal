258eeb4366adfedb62d11eda001370db8b258f01
NitroSniper <mahiemiah35@gmail.com>
2023-01-12 19:00:43 +0000

    Bot is able to access site.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # This is a recode of the selenium script I built with my current knowledge.
 import asyncio
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class BotController():
     def __init__(self, room_code: str) -> None:
@@ -16,19 +19,26 @@ class BotController():
 
         # Try to join the room. 
         self.driver.get(f"https://jklm.fun/{room_code}")
-        self.driver_wait.until(
-                method=expected_conditions.element_to_be_clickable()
+        
+        text_box: WebElement = self.driver_wait.until(
+                method=expected_conditions.element_to_be_clickable(
+                    (By.CLASS_NAME, "styled.nickname")
+                    )
                 )
         
+        # Write User info
+        text_box.clear()
+        text_box.send_keys("Ortin")
+        text_box.send_keys(Keys.ENTER)
 
 
         
     async def answer_question(self) -> None:
-        
+        pass
     
 
 def main() -> None:
-    pass
+    BotController(room_code="TFQT")
 
 
 
